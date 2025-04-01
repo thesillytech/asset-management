c324cb88069314eabb377c8c46a499f900bb81be
BoBo <sanilpsy@gmail.com>
2025-03-31 20:17:05 -0500

    fetched data from database + working on design

--- src/pages/drivers/drivers-common.js
+++ src/pages/drivers/drivers-common.js
@@ -1,8 +1,26 @@
 import Header from '../../components/generic/Header.js'
+import useSWR  from 'swr';
 
 const DriversCommon = () => {
+    const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+    const { data, error } = useSWR('/api/drivers/drivers-common', fetcher)
+
+    // const drivers = data?.results?.map((result) => result).map((data) => console.log('Data', data)) || [];
+
+    const drivers = data?.results
+
+    if(error) return <div>Failed to Load</div>
+    if(!data) return <div>Loading...</div>
+
+
     return (
-        <Header />
+        <>
+            <Header />
+            {drivers.map((driver, index) => (
+                <li key={index}>{JSON.stringify(driver)}</li>
+            ))}
+        </>
     )
 }
 
